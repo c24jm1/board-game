2fcaf12007cec2dfcbfeccc7a9bd2a3d5fbe2942
Julia Miyasaka <c24jm1@dalton.org>
2022-12-12 14:07:32 -0500

    checkeres show up

--- src/index.js
+++ src/index.js
@@ -6,6 +6,22 @@ function Square ({value}) {
 
   console.log(value)
 
+// 8 13 18 23 28 33 38 43
+// 11 16 21 26 31 36 41 46
+// 14 19 24 29 34 39 44 49
+
+
+// 17 22 27 32 37 42 47 52
+// 20 25 30 35 40 45 50 55
+
+// 65 70 75 80 85 90 95 100
+// 68 73 78 83 88 93 98 103
+// 71 76 81 86 91 96 101 106
+
+
+
+
+
   if (value % 2 === 0){
     return (
       <button className={"square white"}>
@@ -13,10 +29,47 @@ function Square ({value}) {
     );
   }
   else if (value%2 ===1){
-    return (
-      <button className={"square black"}>
-      </button>
+
+    if(value < 50 && value %10 ===3){
+      return(
+        <button className = {"square black"}><div className={"checker white"}></div></button>
+      )
+    }
+    else if(value<50 && value%10===1){
+      return(
+        <button className = {"square black"}><div className={"checker white"}></div></button>
+      )
+    }
+    else if (value <50 && value %10 ===9){
+      return(
+        <button className = {"square black"}><div className={"checker white"}></div></button>
+      )
+    }
+    else if(value>55 &&value %10===5){
+      return(
+        <button className = {"square black"}><div className={"checker red"}></div></button>
+      )
+    }
+    else if (value>55 && value %10 ===3){
+      return(
+        <button className = {"square black"}><div className={"checker red"}></div></button>
+      )
+    }
+    else if(value >55 && value %10 ===1){
+      return(
+        <button className = {"square black"}><div className={"checker red"}></div></button>
+      )
+    }
+
+
+
+
+    else{
+      return (
+        <button className={"square black"}>
+          </button>
     );
+  }
   }
 
 
@@ -32,12 +85,11 @@ class Board extends React.Component {
     return (
       <div>
       {
-      	[1,2,3,4,5,6,7,8].map( (row) => {
+      	[1,2,3,4,5,20,21,22].map( (row) => {
 				 return <div key={row} className="board-row">
             {
             	[1,2,3,4,5,6,7,8].map( (col) => {
-               return this.renderSquare(col+row)
-                
+               return this.renderSquare((5*col+3*row))
           		})
             }
             </div>
@@ -52,7 +104,7 @@ class Board extends React.Component {
 class Checker extends React.Component {
   render() {
     return (
-      <var className="checker" />
+      <div className="checker" />
     )
   }
 }
